efd94b774daa2495047863ffe202b7cfc8c46cc5
ak1ya <sasanoha.payapaya@gmail.com>
2021-06-28 19:38:49 +0900

    first commit

---
+++ flask/app/index.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+# TOP Page
+@app.route("/")
+def index():
+    values = { "name": "Taro"}
+    return render_template('index.html', data=values)
+
+# Second Page
+@app.route("/test")
+def test():
+    values = { "message":"Hello! This is test page." }
+    return render_template('test.html', data=values)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80, debug=True)
